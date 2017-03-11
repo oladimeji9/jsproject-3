74c93ddb702406fccb3d10b0652e78a0375d1d57
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-03-11 09:34:05 +0100

    my first commit

---
+++ app/aritGeo.js
@@ -0,0 +1,30 @@
+﻿
+exports.aritGeo = function(arr) {
+
+    var arithmetic = false,
+    geometric = false,
+    d1 = arr[1] - arr[0],
+    d2 = arr[2] - arr[1],
+    r1 = arr[1] / arr[0],
+    r2 = arr[2] / arr[1];
+
+    if (d1 === d2) {
+        arithmetic = true;
+    }
+    if (r1 === r2) {
+        geometric = true;
+    }
+
+    if (arithmetic) {
+        return "Arithmetic";
+    }
+    else if (geometric) {
+        return "Geometric";
+    }
+    else if (arr.length === 0) {
+        return 0;
+    }
+    else if (!arithmetic || !geometric) {
+        return -1;
+    }
+}
